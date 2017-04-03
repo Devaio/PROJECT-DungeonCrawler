c5ad911d621f68da2a6daa249639cc5442cd02eb
Devaio <robert.camp@outlook.com>
2017-04-03 12:57:51 -0600

    sync

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
-import { User } from '../../models/user';
 import { Users } from '../../providers/users';
 
 @Component({
@@ -11,14 +10,58 @@ import { Users } from '../../providers/users';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, users : Users) {
+  rAccount: {email: string, password: string} = {
+    email: '',
+    password: ''
+  };
+  lAccount: {email: string, password: string} = {
+    email: '',
+    password: ''
+  };
 
+  constructor(public navCtrl: NavController, public users : Users, public toastCtrl : ToastController) {
+    if(localStorage.getItem('_spoop')){
+      this.navCtrl.setRoot(TabsPage, {}, {
+        animate : false
+      })
+    }
   }
 
   startApp() {
     this.navCtrl.setRoot(TabsPage, {}, {
       animate: true,
       direction: 'forward'
+    });
+  }
+
+  doLogin() {
+    this.users.login(this.lAccount).subscribe((resp) => {
+      // this.navCtrl.push(MainPage);
+      this.startApp();
+    }, (err) => {
+      // this.navCtrl.push(MainPage);
+      // // Unable to log in
+      let toast = this.toastCtrl.create({
+        message: 'Login Failed',
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    });
+  }
+  doRegister() {
+    this.users.signup(this.rAccount).subscribe((resp) => {
+      // this.navCtrl.push(MainPage);
+      this.startApp();
+    }, (err) => {
+      // this.navCtrl.push(MainPage);
+      // // Unable to log in
+      let toast = this.toastCtrl.create({
+        message: 'Signup Failed',
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
     });
   }
 
--- src/providers/users.ts
+++ src/providers/users.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-
 import { Api } from './api';
-// import {User} from '../models/user';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 
 @Injectable()
 export class Users {
-
+  _user: any;
   constructor(public http: Http, public api: Api) {
   }
 
@@ -17,9 +17,63 @@ export class Users {
     var uid = localStorage.getItem('_spoop'); // User ID
     console.log(char)
     return this.api.get('/api/chars/user/' + uid, params)
-      // .map(resp => resp.json());
+    // .map(resp => resp.json());
   }
 
+  login(accountInfo: any) {
+    let seq = this.api.post('/auth/login', accountInfo);
+    console.log('LOGIN', seq);
+    seq
+      .map(res => res.json())
+      .subscribe(res => {
+        console.log('LOGIN RESP', res);
+        // If the API returned a successful response, mark the user as logged in
+        if (res.status == 'success') {
+          this._loggedIn(res);
+        } else {
+        }
+      }, err => {
+        console.error('ERROR', err);
+      });
 
+    return seq;
+  }
+
+  /**
+   * Send a POST request to our signup endpoint with the data
+   * the user entered on the form.
+   */
+  signup(accountInfo: any) {
+    let seq = this.api.post('/api/users', accountInfo);
+
+    seq
+      .map(res => res.json())
+      .subscribe(res => {
+        // If the API returned a successful response, mark the user as logged in
+        if (res.status == 'success') {
+          this._loggedIn(res);
+        }
+      }, err => {
+        console.error('ERROR', err);
+      });
+
+    return seq;
+  }
+
+  /**
+   * Log the user out, which forgets the session
+   */
+  logout() {
+    this._user = null;
+  }
+
+  /**
+   * Process a login/signup response to store user data
+   */
+  _loggedIn(resp) {
+    console.log('RESPON', resp);
+    this._user = resp;
+    localStorage.setItem('_spoop', resp._id);
+  }
 
 }
